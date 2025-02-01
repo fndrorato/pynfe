ce1545d6e0b9cfb9a0300d43a2ed2520f12a51ff
Fernando Rorato <fernando.rorato@gmail.com>
2025-01-31 23:10:31 -0300

    Update views.py

--- nota_fiscal/views.py
+++ nota_fiscal/views.py
@@ -865,7 +865,7 @@ class NotaFiscalCancelarView(View):
             'motivo': xMotivo,
             'xml_retorno': xml_retorno
         }  
-        return JsonResponse(data_cancelamento)
+        # return JsonResponse(data_cancelamento)
         cancelamento = salvar_cancelamento(data_cancelamento)        
         
         http_status = 404
